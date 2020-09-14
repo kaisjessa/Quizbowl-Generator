cc3621975e216b174f8d14c57b5ae135041cd752
Kais Jessa <kaisjessa@gmail.com>
2020-09-13 22:00:40 -0400

    more changes
    should create a separate dict for answers

--- python_markov2.py
+++ python_markov2.py
@@ -4,7 +4,7 @@ text = open('quizbowl_no_accents.txt', encoding='utf8').read()
 
 corpus = text.split(" ")
 for word in corpus:
-	if word.startswith('&'):
+	if '&' in word:
 		corpus.remove(word)
 #print(sorted(list(set(corpus))))
 
@@ -22,38 +22,34 @@ for word_1, word_2 in pairs:
         word_dict[word_1] = [word_2]
 
 #first_word = np.random.choice(corpus)
+# Generate tossup, ftp, answer
 first_word = "TOSSUP:"
 chain = [first_word]
 n_words = 1000
-
 for i in range(n_words):
     chain.append(np.random.choice(word_dict[chain[-1]]))
-
 tu = ' '.join(chain)
-
 chain = ["For", "10", "points,"]
 n_words = 100
-
 for i in range(n_words):
     chain.append(np.random.choice(word_dict[chain[-1]]))
 
 ftp = ' '.join(chain)
-
 first_word = "ANSWER:"
 chain = [first_word]
 n_words = 100
-
 for i in range(n_words):
     chain.append(np.random.choice(word_dict[chain[-1]]))
-
 an = ' '.join(chain)
 # tu = tu[:min(len(tu)-1, tu.rfind('.'), tu[1:].find('TOSSUP:'), tu.find('ANSWER:'), tu.find('For 10 points,'))]
 # ftp = ftp[:min(ftp.rfind('.'), ftp.find('TOSSUP:'), ftp.find('ANSWER:'))]
 # an = an[:min(an.rfind('.'), an.find('TOSSUP:'), an[1:].find('ANSWER:'))]
+
+# Format tossup, ftp, answer
 tu_list = tu[len("TOSSUP: "):].split('.')
 tu_list2 = []
 for t in tu_list:
-	if("TOSSUP:" not in t and "ANSWER:" not in t and "For 10 points" not in t and len(t)>50):
+	if("TOSSUP:" not in t and "ANSWER:" not in t and "For 10 points" not in t and '[' not in t and ']' not in t and len(t)>50):
 		tu_list2.append(t)
 tu_list2 = tu_list2[:min(len(tu_list2)-1, 4)]
 tu2 = "TOSSUP: " + '.'.join(tu_list2) + '.'
@@ -62,7 +58,7 @@ tu2 = "TOSSUP: " + '.'.join(tu_list2) + '.'
 ftp_list = ftp[len("For 10 points, "):].split('.')
 ftp_list2 = []
 for t in ftp_list:
-	if("TOSSUP:" not in t and "ANSWER:" not in t and "For 10 points" not in t and len(t)>50):
+	if("TOSSUP:" not in t and "ANSWER:" not in t and "For 10 points" not in t and '[' not in t and ']' not in t and len(t)>50):
 		ftp_list2.append(t)
 ftp_list2 = ftp_list2[:min(len(ftp_list2)-1, 1)]
 ftp2 = "For 10 points, " + '.'.join(ftp_list2) + '.'
@@ -73,9 +69,12 @@ an_list2 = []
 for t in an_list:
 	if("TOSSUP:" not in t and "ANSWER:" not in t and "For 10 points" not in t and len(t)>50):
 		an_list2.append(t)
-an_list2 = an_list2[:min(len(an_list2)-1, 1)]
-an2 = "\nANSWER: " + '.'.join(an_list2) + '.'
+if(len(an_list2)==0):
+	an_list2 = ['']
+an2 = "\nANSWER: " + an_list2[0] + '.'
+if('[' in an2 and ']' not in an2):
+	an2 += ']'
 
-
+# Output
 print(tu2, ftp2)
 print(an2)
--- quizbowl_process.py
+++ quizbowl_process.py
@@ -1,6 +1,5 @@
-with open('quizbowl_raw.txt', encoding='utf_8') as f1:
-	with open('quizbowl.txt', 'w', encoding='utf_8') as f2:
+with open('quizbowl_math.txt', encoding='utf_8') as f1:
+	with open('quizbowl_math2.txt', 'w', encoding='utf_8') as f2:
 		for line in f1:
 			if("TOSSUP:" in line or "ANSWER:" in line):
-				f2.write(line)
-				f2.write('\n')
+				f2.write(line + '\n')
